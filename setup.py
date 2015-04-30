30e591bd671a5455a1f1d448769d8f478ccd5ce3
Wietze Helmantel <helmantel@gmail.com>
2015-04-30 20:45:38 +0200

    Changed README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='noise.addon',
       version=version,
       description="Noise",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
